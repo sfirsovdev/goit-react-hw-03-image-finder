4f0316e38a558df8fbce69a48462c5d4a1933271
Cтас <90937465+sfirsovdev@users.noreply.github.com>
2022-07-13 00:35:59 +0300

    Deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,130 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { ToastContainer, toast } from 'react-toastify';
+import { Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import SearhBar from './searchBar/SearchBar';
+import ImageGallery from './image-gallery/ImageGallery';
+import LoadMoreButton from './button/Button';
+import { AppContainer } from './App.styled';
+import fetchApi from '../components/service/ApiService';
+import Spiner from './loader/Loader';
+import Modal from './modal/Modal';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+export default class App extends Component {
+  static propTypes = { searchQuery: PropTypes.string };
+  state = {
+    searchQuery: '',
+    images: [],
+    page: 1,
+    selectedImage: null,
+    alt: null,
+    status: 'idle',
+    error: null,
+  };
+  totalHits = null;
+
+  async componentDidUpdate(_, prevState) {
+    const { page, searchQuery } = this.state;
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.setState({ status: 'pending' });
+
+      try {
+        const imageData = await fetchApi(searchQuery, page);
+        this.totalHits = imageData.total;
+        const imagesHits = imageData.hits;
+        if (!imagesHits.length) {
+          toast.warning(
+            'No results were found for your search, please try something else.',
+            { transition: Zoom, position: 'top-center' }
+          );
+        }
+        this.setState(({ images }) => ({
+          images: [...images, ...imagesHits],
+          status: 'resolved',
+        }));
+      } catch (error) {
+        toast.error(`Sorry something went wrong. ${error.message}`);
+        this.setState({ status: 'rejected' });
+      }
+    }
+  }
+  handleFormSubmit = searchQuery => {
+    if (this.state.searchQuery === searchQuery) {
+      return;
+    }
+    this.resetState();
+    this.setState({ searchQuery });
+  };
+  handleSelectedImage = (largeImageUrl, tags) => {
+    this.setState({
+      selectedImage: largeImageUrl,
+      alt: tags,
+    });
+  };
+
+  resetState = () => {
+    this.setState({
+      searchQuery: '',
+      page: 1,
+      images: [],
+      selectedImage: null,
+      alt: null,
+      status: 'idle',
+    });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+
+    window.scrollTo({
+      top: document.documentElement.scrollHeight - 90,
+      behavior: 'smooth',
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      selectedImage: null,
+    });
+  };
+
+  render() {
+    const { images, status, selectedImage, alt, error } = this.state;
+    return (
+      <AppContainer>
+        <SearhBar onSubmit={this.handleFormSubmit} />
+        <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
+        {status === 'pending' && <Spiner />}
+        {error && (
+          <h1 style={{ color: 'orangered', textAlign: 'center' }}>
+            {error.message}
+          </h1>
+        )}
+        {images.length > 0 && (
+          <ImageGallery
+            images={images}
+            selectedImage={this.handleSelectedImage}
+          />
+        )}
+        {images.length > 0 && images.length !== this.totalHits && (
+          <LoadMoreButton onClick={this.loadMore} />
+        )}
+        {selectedImage && (
+          <Modal
+            selectedImage={selectedImage}
+            tags={alt}
+            onClose={this.closeModal}
+          />
+        )}
+      </AppContainer>
+    );
+  }
+}
+
+
